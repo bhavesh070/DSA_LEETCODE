516a6297f00bc1625b83de88c242b95d4ac0e681
bhavesh070 <bhavesh070alin@gmail.com>
2023-10-01 00:47:42 +0530

    String File added

---
+++ JAVA_Advanced/src/StringClass/FirstClass.java
@@ -0,0 +1,9 @@
+package StringClass;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        int a = 10;
+        String c = " bhavesh";
+        System.out.println(a);
+    }
+}
