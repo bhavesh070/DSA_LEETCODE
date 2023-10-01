8da8debaf6f88c742994ff1cf998468774c7fec6
bhavesh070 <bhavesh070alin@gmail.com>
2023-10-01 20:49:49 +0530

    Sum of N natural Numbers

--- src/GFG/Sum_N.java
+++ src/GFG/Sum_N.java
@@ -4,6 +4,7 @@ public class Sum_N {
     public static void main(String[] args) {
         System.out.println(sum1(5));
         System.out.println(sum2( 5));
+        System.out.println(sum3(5));
 
     }
     public static int sum1(int n){
@@ -13,6 +14,15 @@ public class Sum_N {
         int sum = 0;
         for (int i=0; i<=n;i++){
             sum+=i;
+        }
+        return sum;
+    }
+    public static int sum3(int n){
+        int sum=0;
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=i;j++){
+                sum++;
+            }
         }
         return sum;
     }
