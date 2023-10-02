20ae2b0c3e92639d118d1d6399755737c7e2a96c
bhavesh070 <bhavesh070alin@gmail.com>
2023-10-03 00:19:01 +0530

    Patterns

--- src/GFG/Pattern.java
+++ src/GFG/Pattern.java
@@ -4,6 +4,7 @@ public class Pattern {
     public static void main(String[] args) {
         pattern1(5);
         pattern2(5);
+        pattern3(5);
     }
     public static void pattern1(int n){
         for (int i=1;i<=n;i++){
@@ -16,6 +17,14 @@ public class Pattern {
     public static void pattern2(int n){
         for (int i=1;i<=n;i++){
             for (int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern3(int n){
+        for (int i=n;i>=1;i--){
+            for (int j=1;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
