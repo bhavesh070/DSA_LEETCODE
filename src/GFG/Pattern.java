4801f371e6b4a5d9da8d18b7b1b5a39465c8b91b
bhavesh070 <bhavesh070alin@gmail.com>
2023-10-03 00:04:08 +0530

    Patterns

---
+++ src/GFG/Pattern.java
@@ -0,0 +1,15 @@
+package GFG;
+
+public class Pattern {
+    public static void main(String[] args) {
+        pattern1(5);
+    }
+    public static void pattern1(int n){
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
