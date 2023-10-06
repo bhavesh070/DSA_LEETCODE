0e52a35edb9a74bcd28677b9dc092c3bb796e87d
bhavesh070 <bhavesh070alin@gmail.com>
2023-10-06 23:31:45 +0530

    Second Commit

--- JAVA_Advanced/src/StringClass/FirstClass.java
+++
@@ -1,9 +0,0 @@
-package StringClass;
-
-public class FirstClass {
-    public static void main(String[] args) {
-        int a = 10;
-        String c = " bhavesh";
-        System.out.println(a);
-    }
-}
