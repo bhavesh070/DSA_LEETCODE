264446e3cb51bca8124b4debcc8990fc9353d339
bhavesh070 <bhavesh070alin@gmail.com>
2023-12-27 12:43:22 +0530

    updated the folder

--- src/GFG/BinarySearch/BinarySearch.java
+++ src/GFG/BinarySearch/BinarySearch.java
@@ -1,13 +1,21 @@
 package PACKAGE_NAME.GFG.BinarySearch;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BinarySearch {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int [] arr = {-1,0,3,5,9,12};
         int x = 9 ;
         System.out.println(LinearSearch(arr,x));
         System.out.println(binary(arr,x));
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+        System.out.println("Enter target");
+        int target = sc.nextInt();
+        System.out.println(Bin(arr,target,low,high));
     }
     public static int LinearSearch(int[] arr,int x){
         for (int i=0;i<arr.length;i++){
@@ -32,5 +40,16 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+    public static int Bin(int[] arr, int target, int low, int high){
+        if(low>high)return -1;
+        int mid = (low+high)/2;
+        if(arr[mid]==target){
+            return mid;                 /// RECURSIVE METHOD
+        }else if(arr[mid]>target){
+            return Bin(arr,target,low,mid-1);
+        }else {
+            return Bin(arr,target,mid+1,high);
+        }
     }
 }
