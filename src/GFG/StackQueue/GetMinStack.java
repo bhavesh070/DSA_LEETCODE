c3a5c106b1430fa11a9c935c5c4e6196dd401da0
bhavesh070 <bhavesh070alin@gmail.com>
2024-03-08 12:45:50 +0530

    Updated all the files

---
+++ src/GFG/StackQueue/GetMinStack.java
@@ -0,0 +1,17 @@
+package PACKAGE_NAME.GFG.StackQueue;
+
+import java.util.Stack;
+
+class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+}
+
+public class GetMinStack {
+    public static void main(String[] args) {
+
+    }
+    public void psuh(int val){
+        this.val = val;
+    }
+}
