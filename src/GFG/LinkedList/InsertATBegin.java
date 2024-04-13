53ce3aa7c4c370e8c5680b20a16346404fe1d17c
bhavesh070 <bhavesh070alin@gmail.com>
2024-04-14 03:18:30 +0530

    updated all the files

---
+++ src/GFG/LinkedList/InsertATBegin.java
@@ -0,0 +1,16 @@
+package PACKAGE_NAME.GFG.LinkedList;
+class Node{
+    int data;
+    Node next;
+    Node(int x){
+        this.data = x;
+        this.next=null;
+
+    }
+
+}
+public class InsertATBegin {
+    public static void main(String[] args) {
+
+    }
+}
