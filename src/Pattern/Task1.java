448dd28d7587d6c603b84ed8c3eceec128abd568
bhavesh070 <bhavesh070alin@gmail.com>
2024-05-29 15:50:43 +0530

    updated all the files

--- src/Pattern/Task1.java
+++ src/Pattern/Task1.java
@@ -3,14 +3,24 @@ package PACKAGE_NAME.Pattern;
 public class Task1 {
     public static void main(String[] args) {
         pattern1(4);
+        pater1(5);
     }
 
     public static void pattern1(int n){
         for (int i=1;i<=n;i++){
             for (int j=1;j<=i;j++){
-                System.out.print("* ");
+                System.out.print("0 ");
             }
             System.out.println();
         }
+    }
+    public static void pater1(int n){
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                System.out.print("0 ");
+            }
+            System.out.println();
+        }
+
     }
 }
