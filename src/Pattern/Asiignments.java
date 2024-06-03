8fcb998075017067b8ea67f78bc2778850b84905
bhavesh070 <bhavesh070alin@gmail.com>
2024-06-03 17:52:45 +0530

    update all the files

--- src/Pattern/Asiignments.java
+++ src/Pattern/Asiignments.java
@@ -6,6 +6,7 @@ public class Asiignments {
         pat2(5);
         pat3(5);
         pat4(5);
+        pat5(5);
     }
     static void pat1(int n){
         for (int i=0;i<n;i++){
@@ -39,5 +40,19 @@ public class Asiignments {
             System.out.println();
         }
 
+    }
+    static void pat5(int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<i+1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
