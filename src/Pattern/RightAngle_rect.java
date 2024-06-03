6a5b5b39195867dff5b2e335655a8461a3c3aedc
bhavesh070 <bhavesh070alin@gmail.com>
2024-06-03 16:52:10 +0530

    updated all the files

--- src/Pattern/RightAngle_rect.java
+++ src/Pattern/RightAngle_rect.java
@@ -4,6 +4,7 @@ public class RightAngle_rect {
     public static void main(String[] args) {
         RectPat(4);
         RighAnglePaT(4);
+        RightAngleMirror(4);
     }
     static void RectPat(int n){
         for (int i=0;i<n;i++){
@@ -17,6 +18,19 @@ public class RightAngle_rect {
         for (int i=0;i<n;i++){
             for (int j=0;j<i+1;j++){
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void RightAngleMirror(int n){
+        for (int i=0;i<n;i++) {
+            //for space
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            //for space
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
