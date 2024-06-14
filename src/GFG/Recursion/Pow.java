f2786d1c8b829861402be8d2ec3d65bf8369ade9
bhavesh070 <bhavesh070alin@gmail.com>
2024-06-14 23:39:13 +0530

    updated all the files

--- src/GFG/Recursion/Pow.java
+++ src/GFG/Recursion/Pow.java
@@ -2,9 +2,9 @@ package PACKAGE_NAME.GFG.Recursion;
 
 public class Pow {
     public static void main(String[] args) {
-        System.out.println(mypow(2,-2));
+        System.out.println(myPOW(2,-2));
     }
-    public static double mypow(double x , int n){
+    public static double mypow(double x , int n){   // brute force approach
         if(n == 0){
             return 1;
         }
@@ -17,5 +17,26 @@ public class Pow {
             return 1/ans;
         }
         return ans;
+    }
+    public static double myPOW(double x, int n){
+         // optimized recursive approach
+        if(n==0){
+            return 1;
+        }
+        double ans = 1;
+        Long N = Math.abs((long) n);
+        while (N>0){
+            if(N%2==0){
+                 x = x*x;
+                 N = N/2;
+            }
+            ans = ans*x;
+            N = N-1;
+        }
+        if(n<0){
+            return 1/ans;
+        }
+        return ans;
+
     }
 }
