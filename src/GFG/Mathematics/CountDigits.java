fe6ee6a633f46e82ad4151cdd3d44f0b85ae1547
bhavesh070 <bhavesh070alin@gmail.com>
2024-11-14 21:58:49 +0530

    updated all the files

---
+++ src/GFG/Mathematics/CountDigits.java
@@ -0,0 +1,16 @@
+package PACKAGE_NAME.GFG.Mathematics;
+import java.util.*;
+public class CountDigits{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        while(n>0){
+            n = n / 10;
+            count++;
+        }
+        System.out.println(count);
+
+    }
+
+}
